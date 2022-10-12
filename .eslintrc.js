e0b6abc2ca264cf6652e80a12f4570adf7333df0
a2ron-k <aaronkwah@gmail.com>
2022-10-12 21:26:57 +0800

    debug: fix eslint config and errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,20 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-    indent: [
-      'error',
-      2,
-    ],
-    eqeqeq: 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': [
-      'error', 'always',
-    ],
-    'arrow-spacing': [
-      'error', { before: true, after: true },
-    ],
-    'no-console': 0,
-    'linebreak-style': [
-      'error',
-      'unix',
-    ],
-    quotes: [
-      'error',
-      'single',
-    ],
-    semi: [
-      'error',
-      'never',
-    ],
-  },
+    extends: ['plugin:vue/vue3-recommended'],
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        indent: ['error', 4],
+        eqeqeq: 'error',
+        'no-trailing-spaces': 'error',
+        'object-curly-spacing': ['error', 'always'],
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'no-console': 0,
+        'linebreak-style': ['error', 'unix'],
+        quotes: ['error', 'single'],
+        semi: ['error', 'never'],
+    },
 }
