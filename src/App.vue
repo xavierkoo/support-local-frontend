1f8fd7b2335192b2e7641a737bc87c8e17522b6b
a2ron-k <aaronkwah@gmail.com>
2022-10-12 18:15:25 +0800

    feat: add bootstrap cdn

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
+
 <template>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+  integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  
 	<nav>
 		<router-link to="/">Home</router-link> |
 		<router-link to="/about">About</router-link>
