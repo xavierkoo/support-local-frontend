c6f02332f9e6ec109dac0ff7f1f9e58edf46182f
a2ron-k <aaronkwah@gmail.com>
2022-10-19 00:31:22 +0800

    feat: add product card component

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <div class="card-body">
+            <div
+                class="row mb-1"
+                style="border: none"
+            >
+                <div class="col-xl-3">
+                    <img src="./profilepng.png">
+                </div>
+                <div class="col-xl-9">
+                    <h6 class="mb-0">
+                        Hello World
+                    </h6>
+                    <p class="">
+                        1 hour ago
+                    </p>
+                </div>
+            </div>
+            <div
+                class="row"
+                style="border: none"
+            >
+                <div class="col-xl-8">
+                    <div class="">
+                        Foldable bicycle wa blue whell
+                    </div>
+                    <div class="fst-italic">
+                        Special offer: $21.99
+                    </div>
+                </div>
+                <div class="col-xl-4">
+                    <p class="fw-bold text-end">
+                        S$180
+                    </p>
+                    <p class="text-end">
+                        23 Sold
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ProductCard",
+};
+</script>
+
+<style scoped></style>
