cb0832a820536fc1a19f8d636b9475588a9b98ea
a2ron-k <aaronkwah@gmail.com>
2022-10-22 12:46:08 +0800

    feat: add category btn component

---
+++ src/components/SideNavPill.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="my-1">
+        <a
+            class="btn btn-primary text-center"
+            role="button"
+            style="width: 100%"
+        >
+            <img
+                src="../assets/navSidePlaceholder.png"
+                style="width: 40px"
+                class="me-md-2"
+            >
+            <span class="d-none d-xl-inline">{{ categoryname }}</span></a>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "SideNavPill",
+  props: {
+    categoryname: {
+      type: String,
+      default: "Default Cat Name",
+    },
+    categoryimg: {
+      type: String,
+      default: "../assets/navSidePlaceholder.png'",
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
