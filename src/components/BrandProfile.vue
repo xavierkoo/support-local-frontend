dab1bac0b8ab22a10ab07df8652b4e704f619e3f
a2ron-k <aaronkwah@gmail.com>
2022-10-26 00:30:14 +0800

    feat: add brand and merchant rating component

---
+++ src/components/BrandProfile.vue
@@ -0,0 +1,54 @@
+<template>
+    <div>
+        <div
+            class="card text-center pt-2"
+            style="height: 444px"
+        >
+            <!-- Insert Img here -->
+            <img
+                src="../assets/brandprofilepng.png"
+                alt=""
+                class="d-block mx-auto mt-4"
+                style="width: 30%; height: auto"
+            >
+            <div class="card-body">
+                <h3 class="card-title fw-bold">
+                    {{ merchantName }}
+                </h3>
+                <p class="card-text">
+                    {{ desc }}
+                </p>
+                <button class="btn btn-primary">
+                    Contact me
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "BrandProfile",
+  props: {
+    merchantName: {
+      type: String,
+      default: "HellowWorld",
+    },
+    desc: {
+      type: String,
+      default:
+        "The store's collection is a celebration of everything maximalist - there are face masks with elf ears, iridescent platform shoes",
+    },
+    contact: {
+      type: String,
+      default: "#",
+    },
+    merchantPic: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/MerchantRating.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div
+            class="card text-center pt-2 mt-2"
+            style="height: 267px"
+        >
+            <div class="card-body">
+                <h3 class="card-title fw-bold">
+                    Merchant Rating
+                </h3>
+                <h1>{{ merchantRating }} {{ star }}</h1>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "MerchantRating",
+  props: {
+    merchantRating: {
+      type: Number,
+      default: 5,
+    },
+    star: {
+      type: String,
+      default: "⭐",
+    },
+  },
+};
+</script>
+
+<style scoped></style>
