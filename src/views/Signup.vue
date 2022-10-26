1797a2e61cc81b427956169cab32205baf184c97
Ray Quek <ray.quek@gmail.com>
2022-10-26 15:26:13 +0800

    style: update stylesheet

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -2,78 +2,80 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div class="signUp">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-1" />
-                <div class="col-md-5">
-                    <img
-                        src="https://i.pinimg.com/originals/7f/98/dc/7f98dcad5579ed7fc4d4b88b79ecfea4.jpg"
-                        alt=""
-                        class="shadow"
-                    >
-                </div>
-                <div class="col-md-5 p-5 shadow form1">
-                    <div class="container">
-                        <div class="row">
-                            <h3 class="display-6">
-                                <b>Sign Up</b>
-                            </h3>
-                        </div>
-                        <div class="row">
-                            <form>
-                                <div class="form-group">
-                                    <label for="Email">Email <span>*</span></label>
-                                    <input
-                                        id="emailAdd"
-                                        v-model="state.email"
-                                        type="email"
-                                        class="form-control"
-                                    >
-                                    <span v-if="v$.email.$error">
-                                        {{ v$.email.$errors[0].$message }}
-                                    </span>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="password">Password <span>*</span></label>
-                                    <input
-                                        id="password"
-                                        v-model="state.password.password"
-                                        type="password"
-                                        class="form-control"
-                                    >
-                                    <span v-if="v$.password.$error">
-                                        {{ v$.password.$errors[0].$message }}
-                                    </span>
-                                </div>
-                                <br>
-                                <div class="form-group">
-                                    <label for="cfmpassword">Confirm Password <span>*</span></label>
-                                    <input
-                                        id="cfmpassword"
-                                        v-model="state.password.confirm"
-                                        type="password"
-                                        class="form-control"
-                                    >
-                                    <span v-if="v$.password.$error">
-                                        {{ v$.password.$errors[0].$message }}
-                                    </span>
-                                </div>
-                                <br>
-                                <div class="container buttoncont">
-                                    <button
-                                        type="submit"
-                                        class="btn btn-danger"
-                                        @click="submitForm()"
-                                    >
-                                        Sign Up
-                                    </button>
-                                </div>
-                            </form>
+        <div :id="mode">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-1" />
+                    <div class="col-md-5">
+                        <img
+                            src="https://i.pinimg.com/originals/7f/98/dc/7f98dcad5579ed7fc4d4b88b79ecfea4.jpg"
+                            alt=""
+                            class="shadow"
+                        >
+                    </div>
+                    <div class="col-md-5 p-5 shadow back">
+                        <div class="container">
+                            <div class="row">
+                                <h3 class="display-6">
+                                    <b>Sign Up</b>
+                                </h3>
+                            </div>
+                            <div class="row">
+                                <form>
+                                    <div class="form-group">
+                                        <label for="Email">Email <span>*</span></label>
+                                        <input
+                                            id="emailAdd"
+                                            v-model="state.email"
+                                            type="email"
+                                            class="form-control"
+                                        >
+                                        <span v-if="v$.email.$error">
+                                            {{ v$.email.$errors[0].$message }}
+                                        </span>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="password">Password <span>*</span></label>
+                                        <input
+                                            id="password"
+                                            v-model="state.password.password"
+                                            type="password"
+                                            class="form-control"
+                                        >
+                                        <span v-if="v$.password.$error">
+                                            {{ v$.password.$errors[0].$message }}
+                                        </span>
+                                    </div>
+                                    <br>
+                                    <div class="form-group">
+                                        <label for="cfmpassword">Confirm Password <span>*</span></label>
+                                        <input
+                                            id="cfmpassword"
+                                            v-model="state.password.confirm"
+                                            type="password"
+                                            class="form-control"
+                                        >
+                                        <span v-if="v$.password.$error">
+                                            {{ v$.password.$errors[0].$message }}
+                                        </span>
+                                    </div>
+                                    <br>
+                                    <div class="container buttoncont">
+                                        <button
+                                            type="submit"
+                                            class="mainBtnDesign"
+                                            @click="submitForm()"
+                                        >
+                                            Sign Up
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
+                    <div class="col-md-1" />
                 </div>
-                <div class="col-md-1" />
             </div>
         </div>
     </div>
@@ -167,30 +169,5 @@ export default {
 </script>
 
 <style>
-.container {
-  display: inline;
-  margin: auto;
-}
-img {
-  width: 100%;
-  height: 100%;
-  border-radius: 30px;
-}
-.form1 {
-  padding: 20px;
-  /* border: 1px #c8c8c8 solid; */
-  border-radius: 20px;
-}
-.buttoncont {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-}
-button {
-  width: 100px;
-}
-span {
-  color: red;
-}
+@import "../assets/style/global.css";
 </style>
