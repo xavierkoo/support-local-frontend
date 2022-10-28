4f9c44284b2cd511646475de98c7cf3314bce3d7
Ray Quek <ray.quek@gmail.com>
2022-10-28 18:34:25 +0800

    feat: add background animation component

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import router from "./router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
+import Particles from "vue3-particles";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(Particles).mount("#app");
