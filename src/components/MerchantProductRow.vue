b913c5c3d9a8363083ec88ada22fb8d9e299ca5b
Ray Quek <ray.quek@gmail.com>
2022-11-01 22:15:29 +0800

    Merge branch 'main' of https://github.com/WAD2-T05/support-local

---
+++ src/components/MerchantProductRow.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="row orderBack mx-auto text-center mb-2">
+        <div class="col-sm-3">
+            {{ productName }}
+        </div>
+        <div class="col-sm-3">
+            {{ category }}
+        </div>
+        <div class="col-sm-3">
+            {{ unitPrice }}
+        </div>
+        <div class="col-sm-3">
+            <button
+                class="btn btn-danger me-2"
+                @click="$emit('deleteItem', productObj.id)"
+            >
+                Delete
+            </button>
+            <button class="btn btn-warning">
+                Edit
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "ProductRow",
+  props: {
+    productName: {
+      type: String,
+      default: "HellowWorld",
+    },
+    category: {
+      type: String,
+      default: "Women Category",
+    },
+    unitPrice: {
+      type: Number,
+      default: 99.0,
+    },
+    productObj: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  emits: ["deleteItem"],
+  methods: {},
+};
+</script>
+
+<style scoped></style>
