c74d2f00ccb0255fa96478b8308d5f97fdbb09c6
a2ron-k <aaronkwah@gmail.com>
2022-11-07 00:25:41 +0800

    fix: remove # from root routerlink

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import IndividualBrand from "../views/IndividualBrand.vue";
 import LandingView from "../views/LandingView.vue";
 import ShoppingCart from "../views/ShoppingCart.vue";
@@ -75,7 +75,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
