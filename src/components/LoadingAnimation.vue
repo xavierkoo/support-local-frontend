8bd990e5b8b7f35e390925e8840d06af4ab848c8
Ray Quek <ray.quek@gmail.com>
2022-11-09 20:59:36 +0800

    feat: add loading screen component

---
+++ src/components/LoadingAnimation.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container d-flex justify-content-center align-content-center">
+        <div class="spinner">
+            <img
+                src="../assets/Nobackgroundlogo.png"
+                alt=""
+                class="img-fluid"
+            >
+            <div class="spinner-sector spinner-sector-one" />
+            <div class="spinner-sector spinner-sector-two" />
+            <div class="spinner-sector spinner-sector-three" />
+        </div>
+    </div>
+</template>
